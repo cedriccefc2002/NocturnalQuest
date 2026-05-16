b30299c95a88647ded6b6a1c3935e0660578a1ea
cefc <cedriccefc2002@gmail.com>
2026-05-17 00:14:58 +0800

    fix

--- convert.sh
+++ convert.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 for d in */; do
   echo $d
-  mkdir out
-#   mkdir -p ./out/$d
+  mkdir -p ./out/$d
   i=0;
   for f in ./$d*.mp4; do
     echo $f
     # ffmpeg -i $f -vf "scale=360:-1,fps=12" -c:v libwebp_anim -loop 0 -compression_level 6 -quality 70 -an ./out/$f.webp
-    ffmpeg -i $f ./out/$((i++)).webp
+    ffmpeg -i $f ./out/$d$((i++)).webp
   done
 done
