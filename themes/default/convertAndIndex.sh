3f86b9e979e592ccaf1bfacaf7594d4a904a875b
cefc <cedriccefc2002@gmail.com>
2026-05-17 13:00:42 +0800

    resize

--- themes/default/convertAndIndex.sh
+++ themes/default/convertAndIndex.sh
@@ -3,7 +3,7 @@ for d in */; do
   echo $d
   i=0;
   for f in ./$d*.{jpg,png,webp}; do
-    convert "$f" "$f.webp.converted" && rm "$f"
+    convert "$f" "$f.converted.webp" && rm "$f"
   done
-  i=0; for f in ./$d*.webp.converted; do mv "$f" "./$d$((i++)).webp"; done
+  i=0; for f in ./$d*.converted.webp; do mv "$f" "./$d$((i++)).webp"; done
 done
